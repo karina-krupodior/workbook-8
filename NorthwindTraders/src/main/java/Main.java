58ad47c5e09b6812df59f7ffefec54fbbff0bc83
karina-krupodior <karinakrupodior@gmail.com>
2025-06-10 09:56:32 -0500

    Rows of Information from northwind

--- NorthwindTraders/src/main/java/Main.java
+++ NorthwindTraders/src/main/java/Main.java
@@ -2,41 +2,31 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        //jdbc:mysql://[host][:port]/[database][?propertyName1=propertyValue1]
         String url = "jdbc:mysql://localhost:3306/northwind";
         String username = "root";
         String password = "yearup24";
 
-        //open connection
-        //use a statement to execute
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+        try (
+                Connection connection = DriverManager.getConnection(url, username, password);
+                Statement statement = connection.createStatement();
+                ResultSet results = statement.executeQuery("SELECT productId, productName, unitPrice, unitsInStock FROM products")
+        ) {
             System.out.println("Connected to the database!");
-            // create statement, the statement is tied to the open connection
-            Statement statement = connection.createStatement();
+            System.out.printf("%-5s %-30s %-10s %-10s%n", "Id", "Name", "Price", "Stock");
+            System.out.println("------------------------------------------------------------");
 
-            //define query
-            String query = "SELECT * FROM products";
-            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                int id = results.getInt("productId");
+                String name = results.getString("productName");
+                double price = results.getDouble("unitPrice");
+                int stock = results.getInt("unitsInStock");
 
-            while (results.next()){
-                String productName = results.getString("productName");
-                System.out.println("Product Name");
-                System.out.println(productName);
-                String unitsOnOrder = results.getString("unitsOnOrder");
-
-                if (!unitsOnOrder.equals("0")){
-                    System.out.println("Units On Order");
-
-                    System.out.println(unitsOnOrder);
-                }
+                // Nicely formatted row
+                System.out.printf("%-5d %-30s %-10.2f %-10d%n", id, name, price, stock);
             }
-
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
     }
 }
